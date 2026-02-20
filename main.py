692187a4a7ce7e8ca56197b3e029123314b9c909
kyle-rhoades <kyle.rhoades1@education.nsw.gov.au>
2026-02-20 14:50:01 +1100

    Add rotation to the lander
    Rotate the lander image around the center.
    
    Implemented the rotation using the first answer in this thread: https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'sp
 
 
 # Update the display to show the current state of the game
-def draw_window(lander, thruster_level):
+def draw_window(lander, thruster_level, rotation):
     # Draw window background
     WINDOW.blit(BACKGROUND, (0, 0))
 
@@ -49,32 +49,39 @@ def draw_window(lander, thruster_level):
         lander_image = LANDER_LOW_THRUST_IMAGE
     else:
         lander_image = LANDER_HIGH_THRUST_IMAGE
+
+    # Rotate the lander image around the center
+    # See: https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame
     lander_surface = pygame.transform.rotate(pygame.transform.scale(
-        lander_image, (LANDER_DISPLAY_WIDTH, LANDER_DISPLAY_HEIGHT)), 0)
-    WINDOW.blit(lander_surface, (lander.x, lander.y))
+        lander_image, (LANDER_DISPLAY_WIDTH, LANDER_DISPLAY_HEIGHT)), rotation)
+    new_rect = lander_surface.get_rect(center=lander.center)
+    WINDOW.blit(lander_surface, new_rect)
 
     pygame.display.update()
 
 
-def handle_lander_movement(keys_pressed, lander):
+def handle_lander_movement(keys_pressed, lander, rotation):
     thruster_level = 0
     if keys_pressed[pygame.K_a] and lander.x - VELOCITY > 0:  # LEFT
-        lander.x -= VELOCITY
+        #lander.x -= VELOCITY
+        rotation -= VELOCITY
         thruster_level = 2
     if keys_pressed[pygame.K_d] and lander.x + VELOCITY + lander.width < WIDTH:  # RIGHT
-        lander.x += VELOCITY
+        #lander.x += VELOCITY
         thruster_level = 2
+        rotation += VELOCITY
     if keys_pressed[pygame.K_w] and lander.y - VELOCITY > 0:  # UP
         lander.y -= VELOCITY
         thruster_level = 2
     if keys_pressed[pygame.K_s] and lander.y + VELOCITY + lander.height < HEIGHT:  # DOWN
         lander.y += VELOCITY
         thruster_level = 2
-    return thruster_level
+    return thruster_level, rotation
 
 # Main lunar lander game function
 def lunar_lander():
-    lander = pygame.Rect(WIDTH//2 - LANDER_WIDTH//2, 20, LANDER_WIDTH, LANDER_HEIGHT)
+    lander = pygame.Rect(WIDTH//2 - LANDER_WIDTH//2, LANDER_HEIGHT, LANDER_WIDTH, LANDER_HEIGHT)
+    rotation = 0
 
     # Game loop
     clock = pygame.time.Clock()
@@ -90,10 +97,10 @@ def lunar_lander():
                 sys.exit()
 
         keys_pressed = pygame.key.get_pressed()
-        thruster_level = handle_lander_movement(keys_pressed, lander)
+        thruster_level, rotation = handle_lander_movement(keys_pressed, lander, rotation)
 
         # Update the display to show the current state of the game
-        draw_window(lander, thruster_level)
+        draw_window(lander, thruster_level, rotation)
 
     # Play again
     lunar_lander()
